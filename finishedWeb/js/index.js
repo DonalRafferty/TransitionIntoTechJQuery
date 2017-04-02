182e4a6d7da81745906e6c489e510c14fee93669
Donal Rafferty <donal.rafferty@mastercard.com>
2017-04-02 20:16:45 +0100

    Adding example finished website

--- finishedWeb/js/index.js
+++ finishedWeb/js/index.js
@@ -1,14 +1,57 @@
+
+var bounceBall = function(){
+	var time = 500;
+	for (i = 0; i < 5; i++) { 
+    	$(".ball").animate({
+        	marginTop: '+=140px'
+    	}, time);
+    	$(".ball").animate({
+        	marginTop: '-=140px'
+    	}, time);
+	}
+};
+
+var animatePug = function(){
+	var time = 500;
+	for (i = 0; i < 6; i++) { 
+		$('#pug-img').animate({
+		    width: '-=50px',
+		    height: '-=50px'
+		}, time);
+		 $('#pug-img').animate({
+		    width: '+=50px',
+		    height: '+=50px'
+		}, time);
+	}
+}
+
 $(document).ready(function(){
 
-   // jQuery methods go here...
-  	$('#pug-img').click(function(){
-  		$('#pug-img').hide(1000, function(){
-  			$('#pug-img').show();
-  		});
-	});
+   	// jQuery methods go here...
+  	$('#pug-img').fadeIn(3000);
+  	$('#pug-list').slideDown(3000);
+
+  	$("#more").click(function(){
+        $.get("./data/list.txt", function(data, status){
+        	$('#more').hide();
+            $("#food-list").html(data);
+            $("#food-list").show();
+            $(".ball").hide();
+        });
+    });
+
+    $("#bounce").click(function(){
+    	$(".ball").show();
+    	$('#more').show();
+    	$("#food-list").hide();
+    	bounceBall();
+    });
+
+    $('#pug-img').click(function(){
+    	animatePug();
+    });
 
   	var date = new Date().getFullYear();
-
-	$('footer').append(' ' + date);
+	$('footer').append(date);
 
 });
