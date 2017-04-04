6595fd02ec0d4d812532c257f3455a3e75f4a16b
Donal Rafferty <donal.rafferty@mastercard.com>
2017-04-04 16:54:15 +0100

    Adding Menu example

---
+++ exampleMenu/README.md
@@ -0,0 +1,3 @@
+# JQuery - Example Menu Code
+
+This is just an example of how to build a basic menu with jQuery effects
---
+++ exampleMenu/js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+   	// jQuery methods go here...
+    $('.nav li').hover(
+      function () { //appearing on hover
+        $('ul', this).fadeIn();
+      },
+      function () { //disappearing on hover
+        $('ul', this).fadeOut();
+      }
+    );
+
+});
